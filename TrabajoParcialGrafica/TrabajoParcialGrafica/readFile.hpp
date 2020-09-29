72579b44782e7c7ef205900c328f15c5912606a5
Drexlarr <diegodba29@gmail.com>
2020-09-28 23:13:32 -0500

    Cargar textura y graficar 2d terminado

--- TrabajoParcialGrafica/TrabajoParcialGrafica/readFile.hpp
+++ TrabajoParcialGrafica/TrabajoParcialGrafica/readFile.hpp
@@ -9,6 +9,7 @@
 #include <glm/matrix.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "stb_image.h"
 
 using namespace std;
 
@@ -240,6 +241,34 @@ struct Figure {
 		else
 			glDrawArrays(GL_TRIANGLES, 0, this->nVertices);
 	}
+
+	void loadTexture(string path) {
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture); 
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		
+		int width, height, nrChannels;
+
+		unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+	}
+
 };
 
 void read(std::string nameFile, Figure* figure, bool texture, bool rgb) {
